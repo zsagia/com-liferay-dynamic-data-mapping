inacionery/liferay-portal

LPS-65648 Regenerate

modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMStructureLinkLocalService.java
@@ -160,6 +160,8 @@ public interface DDMStructureLinkLocalService extends BaseLocalService,
 	public PersistedModel getPersistedModel(Serializable primaryKeyObj)
 		throws PortalException;
 
+	public int countByC_C(long classNameId, long classPK);
+
 	/**
 	* Returns the number of d d m structure links.
 	*
@@ -236,8 +238,16 @@ public interface DDMStructureLinkLocalService extends BaseLocalService,
 		long classPK) throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<DDMStructure> getStructureLinkStructures(long classNameId,
+		long classPK, int start, int end) throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<DDMStructureLink> getStructureLinks(long classNameId,
 		long classPK);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<DDMStructureLink> getStructureLinks(long classNameId,
+		long classPK, int start, int end);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<DDMStructureLink> getStructureLinks(long structureId);

modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMStructureLinkLocalServiceUtil.java
@@ -169,6 +169,10 @@ public class DDMStructureLinkLocalServiceUtil {
 		return getService().getPersistedModel(primaryKeyObj);
 	}
 
+	public static int countByC_C(long classNameId, long classPK) {
+		return getService().countByC_C(classNameId, classPK);
+	}
+
 	/**
 	* Returns the number of d d m structure links.
 	*
@@ -264,9 +268,21 @@ public class DDMStructureLinkLocalServiceUtil {
 		return getService().getStructureLinkStructures(classNameId, classPK);
 	}
 
+	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> getStructureLinkStructures(
+		long classNameId, long classPK, int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .getStructureLinkStructures(classNameId, classPK, start, end);
+	}
+
 	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructureLink> getStructureLinks(
 		long classNameId, long classPK) {
 		return getService().getStructureLinks(classNameId, classPK);
+	}
+
+	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructureLink> getStructureLinks(
+		long classNameId, long classPK, int start, int end) {
+		return getService().getStructureLinks(classNameId, classPK, start, end);
 	}
 
 	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructureLink> getStructureLinks(

zsagia/com-liferay-dynamic-data-mapping

dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMStructureLinkLocalService.java
@@ -168,6 +168,9 @@ public interface DDMStructureLinkLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getDDMStructureLinksCount();
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public int getStructureLinksCount(long classNameId, long classPK);
+
 	/**
 	* Returns the OSGi service identifier.
 	*
@@ -236,8 +239,16 @@ public interface DDMStructureLinkLocalService extends BaseLocalService,
 		long classPK) throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<DDMStructure> getStructureLinkStructures(long classNameId,
+		long classPK, int start, int end) throws PortalException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<DDMStructureLink> getStructureLinks(long classNameId,
 		long classPK);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<DDMStructureLink> getStructureLinks(long classNameId,
+		long classPK, int start, int end);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public List<DDMStructureLink> getStructureLinks(long structureId);

dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMStructureLinkLocalServiceUtil.java
@@ -178,6 +178,10 @@ public class DDMStructureLinkLocalServiceUtil {
 		return getService().getDDMStructureLinksCount();
 	}
 
+	public static int getStructureLinksCount(long classNameId, long classPK) {
+		return getService().getStructureLinksCount(classNameId, classPK);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*
@@ -264,9 +268,21 @@ public class DDMStructureLinkLocalServiceUtil {
 		return getService().getStructureLinkStructures(classNameId, classPK);
 	}
 
+	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> getStructureLinkStructures(
+		long classNameId, long classPK, int start, int end)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .getStructureLinkStructures(classNameId, classPK, start, end);
+	}
+
 	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructureLink> getStructureLinks(
 		long classNameId, long classPK) {
 		return getService().getStructureLinks(classNameId, classPK);
+	}
+
+	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructureLink> getStructureLinks(
+		long classNameId, long classPK, int start, int end) {
+		return getService().getStructureLinks(classNameId, classPK, start, end);
 	}
 
 	public static java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructureLink> getStructureLinks(