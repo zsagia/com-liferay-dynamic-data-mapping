79a5cc7cbc7a2f7fad92263718de195773b852e4
Marcellus Tavares <marcellustavares@gmail.com>
2017-06-22 16:36:45 -0300

    LPS-73216 Add Password field type implementation

---
+++ dynamic-data-mapping-type-password/src/main/java/com/liferay/dynamic/data/mapping/type/text/internal/PasswordDDMFormFieldTemplateContextContributor.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.text.internal;
+
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTemplateContextContributor;
+import com.liferay.dynamic.data.mapping.model.DDMFormField;
+import com.liferay.dynamic.data.mapping.model.LocalizedValue;
+import com.liferay.dynamic.data.mapping.model.Value;
+import com.liferay.dynamic.data.mapping.render.DDMFormFieldRenderingContext;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(
+	immediate = true, property = "ddm.form.field.type.name=password",
+	service = {
+		DDMFormFieldTemplateContextContributor.class,
+		PasswordDDMFormFieldTemplateContextContributor.class
+	}
+)
+public class PasswordDDMFormFieldTemplateContextContributor
+	implements DDMFormFieldTemplateContextContributor {
+
+	@Override
+	public Map<String, Object> getParameters(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		Map<String, Object> parameters = new HashMap<>();
+
+		parameters.put(
+			"placeholder",
+			getPlaceholder(ddmFormField, ddmFormFieldRenderingContext));
+		parameters.put(
+			"tooltip", getTooltip(ddmFormField, ddmFormFieldRenderingContext));
+
+		return parameters;
+	}
+
+	protected String getPlaceholder(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		LocalizedValue placeholder = (LocalizedValue)ddmFormField.getProperty(
+			"placeholder");
+
+		return getValueString(
+			placeholder, ddmFormFieldRenderingContext.getLocale());
+	}
+
+	protected String getTooltip(
+		DDMFormField ddmFormField,
+		DDMFormFieldRenderingContext ddmFormFieldRenderingContext) {
+
+		LocalizedValue tooltip = (LocalizedValue)ddmFormField.getProperty(
+			"tooltip");
+
+		return getValueString(
+			tooltip, ddmFormFieldRenderingContext.getLocale());
+	}
+
+	protected String getValueString(Value value, Locale locale) {
+		if (value != null) {
+			return value.getString(locale);
+		}
+
+		return StringPool.BLANK;
+	}
+
+}
---
+++ dynamic-data-mapping-type-password/src/main/java/com/liferay/dynamic/data/mapping/type/text/internal/PasswordDDMFormFieldType.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.type.text.internal;
+
+import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
+import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeSettings;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(
+	immediate = true,
+	property = {
+		"ddm.form.field.type.display.order:Integer=9",
+		"ddm.form.field.type.icon=password-policies",
+		"ddm.form.field.type.js.class.name=Liferay.DDM.Field.Password",
+		"ddm.form.field.type.js.module=liferay-ddm-form-field-password",
+		"ddm.form.field.type.label=password-field-type-label",
+		"ddm.form.field.type.name=password", "ddm.form.field.type.system=true"
+	},
+	service = DDMFormFieldType.class
+)
+public class PasswordDDMFormFieldType extends BaseDDMFormFieldType {
+
+	@Override
+	public Class<? extends DDMFormFieldTypeSettings>
+		getDDMFormFieldTypeSettings() {
+
+		return PasswordDDMFormFieldTypeSettings.class;
+	}
+
+	@Override
+	public String getName() {
+		return "password";
+	}
+
+}
