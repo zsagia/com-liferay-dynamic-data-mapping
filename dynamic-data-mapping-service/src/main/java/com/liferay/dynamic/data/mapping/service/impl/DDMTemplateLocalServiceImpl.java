1c4627795db143f45d9bb120d3d9e5d15a9a9462
Marcellus Tavares <marcellustavares@gmail.com>
2017-06-30 11:00:15 -0300

    LPS-72538 Add new operation to retrieve the templatesCount by groupIds, classNameId, classPK

--- dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMTemplateLocalServiceImpl.java
+++ dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMTemplateLocalServiceImpl.java
@@ -522,7 +522,6 @@ public class DDMTemplateLocalServiceImpl
 	 *         search in the search
 	 * @return the matching template, or <code>null</code> if a matching
 	 *         template could not be found
-	 * @throws PortalException if a portal exception occurred
 	 */
 	@Override
 	public DDMTemplate fetchTemplate(
@@ -896,6 +895,24 @@ public class DDMTemplateLocalServiceImpl
 	public int getTemplatesCount(long groupId, long classNameId, long classPK) {
 		return ddmTemplatePersistence.countByG_C_C(
 			groupId, classNameId, classPK);
+	}
+
+	/**
+	 * Returns the number of templates matching the group IDs, class name ID,
+	 * and class PK.
+	 *
+	 * @param  groupIds the primary keys of the groups
+	 * @param  classNameId the primary key of the class name for the template's
+	 *         related model
+	 * @param  classPK the primary key of the template's related entity
+	 * @return the number of matching templates
+	 */
+	@Override
+	public int getTemplatesCount(
+		long[] groupIds, long classNameId, long classPK) {
+
+		return ddmTemplatePersistence.countByG_C_C(
+			groupIds, classNameId, classPK);
 	}
 
 	@Override
